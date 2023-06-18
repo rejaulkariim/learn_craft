09c73bc24ca365fcf8229e2c73f65c0a29bf9977
karimscoding <rejalkarim66666@gmail.com>
2023-06-18 15:36:17 +0800

    added footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,9 @@
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="py-10 bg-black text-center text-gray-500">
+      <p>&copy; {new Date().getFullYear()}, LearnCraft all right reserved</p>
+    </div>
   )
 }
 
